053baf63bc318d1044788c3a9c97da52d86d2bfd
Kevin Larsen <klarsen.sales@gmail.com>
2013-02-17 16:40:46 -0500

    Update mqconvert.php
    added comments to PHP code

--- mqconvert.php
+++ mqconvert.php
@@ -32,21 +32,29 @@
 		<h1>CSS3 Media Query Converter</h1>
 		<h4>(Marcotte Calculator)</h4>
 <?php 	
+// checks for null input
 	if (($_POST[context] == "" || $_POST[context] == "0") || ($_POST[target] == "" || $_POST[target] == "0")) {
+// null input error handling
 		echo "<font color='#FF0000'><h4>Oops, you seem to be missing something!</h4><h4>Please enter non-zero decimal values for \"target\" and \"context,\"  then click \"Calculate\" to try again...</h4></font>";
 		$target = "";
 		$context = "";
 		$result = "";
+// default output units (radio button) form code block
 		$unit_select = "<input type='radio' name='unit' value='em' checked>em<br /><input type='radio' name='unit' value=\"%\">percent<br /><br />";
 	} else { 
+// sets value of target and context variables from HTTP POST
 		$target = $_POST[target];
 		$context = $_POST[context];	
+		
+// input validity check, ensuring a numeric value was entered in each input field
 		if (is_numeric($target) && is_numeric($context)){
 			if ($_POST[unit] == "em"){
 				$result = $target / $context;
 				$result .= "em";
+// creates radio button form code block based on checked "unit" value from HTTP POST
 				$unit_select = "<input type='radio' name='unit' value='em' checked>em<br /><input type='radio' name='unit' value=\"%\">percent";
 			} else {
+// non-numeric input error handling
 				$result = (($target / $context) * 100);
 				$result .= "%";
 				$unit_select = "<input type='radio' name='unit' value='em'>em<br /><input type='radio' name='unit' value=\"%\" checked>percent";
@@ -58,9 +66,9 @@
 			echo "<font color='#FF0000'><h4>Aw snap, that ain't a number! Ain't nobody got time for that!</h4></font>";
 		}	
 	}
-	
+// creates form code block, including target, context, and unit_select (radio button form code) variable contents
 	$form = "<form method='POST' action=\"$_SERVER[PHP_SELF]\">Target Value (px):<br /><input name='target' type='text' width='10' value=".$target."><br /><br />Context Value (px):<br /><input name='context' type='text' width='10' value=".$context."><br /><br /><b>Output Options:</b><br />".$unit_select."<br /><br /><input type='submit' value='Calculate'><input type='reset' value='Reset'></form><br />";
-	
+// displays form code block along with the formatted output of the calculation
 	echo $form."<b>Output: </b>".$result;
 ?>
 	</body>
