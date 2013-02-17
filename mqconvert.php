d5615dcb2310b76256f01f804cd957b24a1b1faa
Kevin Larsen <klarsen.sales@gmail.com>
2013-02-17 16:33:27 -0500

    Update mqconvert.php
    Added comments with general and licensing information.

--- mqconvert.php
+++ mqconvert.php
@@ -1,6 +1,34 @@
 <html>
   <head><title>CSS3 Media Query Converter</title></head>
 	<body>
+		
+	<!-- 
+		 **** CSS3 Media Query Converter (Marcotte Calculator) **** 
+		 
+		 This tool allows for the easy calculation of css element sizes in 
+		 em/percent by way of the Marcotte Formula (Target / Context = Result) 
+		 
+		 This is useful in the creation of "device independent" responsive 
+		 design stylesheets and media queries. 
+		 
+		 **** Licensing ****
+		 
+		 Copyright Kevin Larsen, 2013 
+		 
+		 This program is free software: you can redistribute it and/or modify
+		 it under the terms of the GNU General Public License as published by
+		 the Free Software Foundation, either version 3 of the License, or
+		 (at your option) any later version.
+
+		 This program is distributed in the hope that it will be useful,
+		 but WITHOUT ANY WARRANTY; without even the implied warranty of
+		 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		 GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	-->
+	
 		<h1>CSS3 Media Query Converter</h1>
 		<h4>(Marcotte Calculator)</h4>
 <?php 	
