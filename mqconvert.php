935ce9b9062992f61f409bc0f6eabb28ed10f815
Kevin Larsen <klarsen.sales@gmail.com>
2013-02-18 08:27:14 -0500

    Update mqconvert.php
    replaced apostrophes with escaped quotes

--- mqconvert.php
+++ mqconvert.php
@@ -35,12 +35,12 @@
 // checks for null input
 	if (($_POST[context] == "" || $_POST[context] == "0") || ($_POST[target] == "" || $_POST[target] == "0")) {
 // null input error handling
-		echo "<font color='#FF0000'><h4>Oops, you seem to be missing something!</h4><h4>Please enter non-zero decimal values for \"target\" and \"context,\"  then click \"Calculate\" to try again...</h4></font>";
+		echo "<font color=\"#FF0000\"><h4>Oops, you seem to be missing something!</h4><h4>Please enter non-zero decimal values for \"target\" and \"context,\"  then click \"Calculate\" to try again...</h4></font>";
 		$target = "";
 		$context = "";
 		$result = "";
 // default output units (radio button) form code block
-		$unit_select = "<input type='radio' name='unit' value='em' checked>em<br /><input type='radio' name='unit' value=\"%\">percent<br /><br />";
+		$unit_select = "<input type=\"radio\" name=\"unit\" value=\"em\" checked>em<br /><input type=\"radio\" name=\"unit\" value=\"%\">percent<br /><br />";
 	} else { 
 // sets value of target and context variables from HTTP POST
 		$target = $_POST[target];
@@ -52,24 +52,24 @@
 				$result = $target / $context;
 				$result .= "em";
 // creates radio button form code block based on checked "unit" value from HTTP POST
-				$unit_select = "<input type='radio' name='unit' value='em' checked>em<br /><input type='radio' name='unit' value=\"%\">percent";
+				$unit_select = "<input type=\"radio\" name=\"unit\" value=\"em\" checked>em<br /><input type=\"radio\" name=\"unit\" value=\"%\">percent";
 			} else {
 // calculates result in percent value
 				$result = (($target / $context) * 100);
 				$result .= "%";
 // creates radio button form code block based on checked "unit" value from HTTP POST
-				$unit_select = "<input type='radio' name='unit' value='em'>em<br /><input type='radio' name='unit' value=\"%\" checked>percent";
+				$unit_select = "<input type=\"radio\" name=\"unit\" value=\"em\">em<br /><input type=\"radio\" name=\"unit\" value=\"%\" checked>percent";
 			}
 		} else {
 // non-numeric input error handling
 			$target = "";
 			$context = "";
 			$result = "";
-			echo "<font color='#FF0000'><h4>Please enter only numeric values!</h4></font>";
+			echo "<font color=\"#FF0000\"><h4>Please enter only numeric values!</h4></font>";
 		}	
 	}
 // displays form code along with the formatted output of the calculation
-	echo "<form method='POST' action=\"$_SERVER[PHP_SELF]\">Target Value (px):<br /><input name='target' type='text' width='10' value=".$target."><br /><br />Context Value (px):<br /><input name='context' type='text' width='10' value=".$context."><br /><br /><b>Output Options:</b><br />".$unit_select."<br /><br /><input type='submit' value='Calculate'><input type='reset' value='Reset'></form><br /><b>Output: </b>".$result;
+	echo "<form method=\"POST\" action=\"$_SERVER[PHP_SELF]\">Target Value (px):<br /><input name=\"target\" type=\"text\" width=\"10\" value=".$target."><br /><br />Context Value (px):<br /><input name=\"context\" type=\"text\" width=\"10\" value=".$context."><br /><br /><b>Output Options:</b><br />".$unit_select."<br /><br /><input type=\"submit\" value=\"Calculate\"><input type=\"reset\" value=\"Reset\"></form><br /><b>Output: </b>".$result;
 ?>
 	</body>
 </html>
